a44fdbe857a0c4a9e1bc5178a9f38254c158168f
Johan Kremer <johan.h.kremer@gmail.com>
2026-03-15 22:45:02 +0100

    add examples

--- script/index.js
+++ script/index.js
@@ -12,28 +12,50 @@
 // Inte DRY, hur kan vi effektivisera detta?
 
 /* 
-for (start; villkor; uppdatering) {
+for (loop variabel; villkor; uppdatering) {
     vad ska hända vid varje loop?
  }
 */
 
-//for loop
+//!for loop (Används oftast när du redan vet ungefär hur många gånger något ska köras.)
 
-for (let i = 0; i <= 10; i++) {
-    console.log(i)
-    console.log(i + 10)
-    console.log("Hej hopp vi kan räkna")
+for (let i = 0; i < 10; i++) {
+    console.log("🔄 for loop", i)
 }
+
+// i = i + 1 eller i += (skriver vi så att variabeln i ska ändras) eller i++
+
+// i = 0; i < 10 eller i = 1; i <= 10
 
 // ++ är samma som += 1
 
-//while loop
+// Utan i++ så hamnar vi i en infinie loop
 
-let i = 1
+//!while loop (Används när du vill köra så länge ett villkor är sant, men du inte nödvändigtvis vet i förväg hur många varv det blir.)
 
-while (i <= 5) {
-    console.log(i)
-    i++
+//Ligger utanför scope
+let number = 0
+
+while (number < 10) {
+
+    if (number <= 0) {
+        console.log("starta räkningen!")
+    }
+
+    console.log("🔄 while loop", number)
+
+    if (number >= 10) {
+        console.log("Vi är i mål!")
+    }
+
+    number++
 }
 
-// Utan i++ så hamnar vi i en infinie loop
+//!do-while loop (Fungerar nästan som while, men den kör alltid minst en gång eftersom villkoret kollas efteråt.)
+
+let i = 0
+
+do {
+    console.log("🔄 Do while loop", i)
+    i++
+} while (i < 5)
