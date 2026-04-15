f94086e05ac009150dd1e2d9be8b552cbcd0acb9
Johan Kremer <johan.h.kremer@gmail.com>
2026-04-15 19:35:45 +0200

    add examples for promises

--- script/index.js
+++ script/index.js
@@ -1,71 +1,58 @@
-/* Sync */
+//Synkront, rad för rad, blockerande 🛑
+console.log("Synkron räkning")
+console.log("------------")
+console.log("1")
+console.log("2")
+console.log("3")
 
-const syncCoffee = "☕️"
 
-console.log(`${syncCoffee}`)
-
-let asyncCoffee
-
-function makeCoffee() {
-    console.log('Start coffee maker 🟢')
-
-    //En asynkron funktion => 💥 side effect 
-    setTimeout(() => {
-        asyncCoffee = "☕️"
-    }, 2000)
-
-    console.log("Your coffee i being made")
-}
-
-makeCoffee()
-console.log(`Your coffee is ${asyncCoffee}`)
-
-console.log("Fixa en macka 🥙 medan jag väntar på mitt ☕️")
+//Asynkront, sätt igång en process och gå vidare till nästa, ej blockerande ✅
+console.log("Asynkron räkning")
+console.log("------------")
+console.log("1")
 
 setTimeout(() => {
-    console.log(`Your delayed ${asyncCoffee} is ready`)
-}, 2500)
+    console.log("2")
+}, 0)
 
-/* Vad är en 💥 side effects och 💎 pure functions? */
+console.log("3")
 
-//Pure funtion (same input/output for given input)
-function numbers(a, b) {
-    return a + b
-}
+//Callback Hell 👹🔥
 
-const result = numbers(1, 2)
-
-//console.log inne i funktionen skulle gör att den inte längre vore "pure"
-console.log(result)
-
-
-/* Callback functions */
-
-//Sync
-
-function sendMessage(printMessage) {
-    const message = "Call me mr. Callback"
-    printMessage(message)
-}
-
-function logMessage(message) {
-    console.log(message)
-}
-
-sendMessage(logMessage)
-
-//Async
-
-const cookRamen = (onRamenReady) => {
+const step1 = (callback) => {
     setTimeout(() => {
-        const ramen = "🍲"
-        onRamenReady(ramen)
-    }, 2000)
+        console.log("Steg 1 klart")
+        callback()
+    }, 500)
 }
 
-const eatRamen = (ramen) => {
-    console.log(`Eat ${ramen}`)
+const step2 = (callback) => {
+    setTimeout(() => {
+        console.log("Steg 2 klart")
+        callback()
+    }, 500)
 }
 
-cookRamen(eatRamen)
-console.log("Do something else while waiting")
+const step3 = (callback) => {
+    setTimeout(() => {
+        console.log("Steg 3 klart")
+        callback()
+    }, 500)
+}
+
+const step4 = (callback) => {
+    setTimeout(() => {
+        console.log("Steg 4 klart")
+        callback()
+    }, 500)
+}
+
+step1(() => {
+    step2(() => {
+        step3(() => {
+            step4(() => {
+                console.log("Alla steg klara")
+            })
+        })
+    })
+})
---
+++ script/promises.js
@@ -0,0 +1,109 @@
+//Promises 👼🌞
+
+function makeRamen() {
+    return new Promise(function (resolve, reject) {
+        const hasNoodles = true
+
+        setTimeout(() => {
+            console.log("Trying to make ramen...")
+            if (hasNoodles) {
+                resolve("🍜")
+            } else {
+                reject("🛑 No noodles left")
+            }
+        }, 1000)
+    })
+}
+
+// const ramenPromise = makeRamen()
+
+// console.log(ramenPromise)
+
+// setTimeout(() => {
+//     console.log(ramenPromise)
+// }, 1500)
+
+function boilEgg(ramen) {
+    return new Promise(function (resolve, reject) {
+        const hasEgg = true
+
+        setTimeout(() => {
+            console.log("Trying to boil egg...")
+            if (hasEgg) {
+                resolve(ramen + " 🥚")
+            } else {
+                reject("🛑 All the chickens are dead")
+            }
+        }, 1000)
+    })
+}
+
+function eatRamen(ramen) {
+    console.log("Lets eat ramen 🍜🎉")
+}
+
+function onSuccess(data) {
+    console.log("We made:", data)
+}
+
+function onError(error) {
+    console.log("We couldn't make ramen because:", error)
+}
+
+makeRamen()
+    .then(boilEgg)
+    .then(onSuccess)
+    .catch(onError)
+    .finally(eatRamen)
+
+// .then((ramen) => {
+//     return boilEgg(ramen).catch(() => {
+//         return ramen
+//     })
+// })
+
+//Ytterligare ett exempel
+
+const step1 = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Steg 1 klart")
+            resolve()
+        }, 2500)
+    })
+}
+
+const step2 = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Steg 2 klart")
+            resolve()
+        }, 500)
+    })
+}
+
+const step3 = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Steg 3 klart")
+            resolve()
+        }, 500)
+    })
+}
+
+const step4 = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Steg 4 klart")
+            resolve()
+        }, 500)
+    })
+}
+
+step1()
+    .then(step2)
+    .then(step3)
+    .then(step4)
+    .then(() => {
+        console.log("Alla steg klara")
+    })
